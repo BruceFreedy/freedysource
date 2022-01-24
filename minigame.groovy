91a2237b75121e978c6fa80d50278341dc49c7c6
Bruce0203 <BruceFreedy@gmail.com>
2022-01-24 20:04:48 +0900

    commit

--- minigame.groovy
+++
@@ -1,7 +0,0 @@
-@command
-{
-    player.print=!message.toLowerCase.startWith='/gamestart'
-    if(!message.toLowerCase.startWith='/gamestart') return  //게임시작 명령어가 아니라면 몸통을 벗어납니다
-    player.print='gamestarted'
-
-}
