a6abcf8fede5c76ca093f4edac32f9287d699d77
Bruce0203 <BruceFreedy@gmail.com>
2022-01-26 14:59:15 +0900

    commit

--- mg.py
+++ mg.py
@@ -19,10 +19,11 @@ online = []
 
 @command
 {
-    args = message.split(" ")                    # 명령어를 공백을 기준으로 나눕니다
+    args = message.split(" ")                    # 명령어를 공백을 기준으 로 나눕니다
     command = args.get(0)
     if (command.lowerCase != "/gamestart") return  # 명령어가 게임시작이 아니면 함수를 즉시 끝냅니다
     if (online.contains(player)) return player.print = "&c이미 게임에 참여하고 있습니다"
+    player.print=상태
     if (상태 != 대기) return player.print = "&c이미 게임이 시작되서 참여할 수 없습니다"
     online.add(player)
     online.each=e.player.print="&6" + player.name + "이(가) " + 게임이름 + "에 참여했습니다"
