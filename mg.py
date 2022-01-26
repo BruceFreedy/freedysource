e9df5677b3cbe1d9dae94f70b766ade82186b38f
Bruce0203 <BruceFreedy@gmail.com>
2022-01-26 15:54:36 +0900

    commit

--- mg.py
+++ mg.py
@@ -27,10 +27,9 @@ online = []
     if (command.lowerCase != "/gamestart") return  # 명령어가 게임시작이 아니면 함수를 즉시 끝냅니다
     if (online.contains(player)) return player.print = "&c이미 게임에 참여하고 있습니다"
     player.print="&a" + 상태
-    player.print="&a" + 대기
     if (상태 != 대기) return player.print = "&c이미 게임이 시작되서 참여할 수 없습니다"
     online.add(player)
-    online.each=e.player.print="&6" + player.name + "이(가) " + 게임이름 + "에 참여했습니다"
+    online.each=print="&6" + player.name + "이(가) " + 게임이름 + "에 참여했습니다"
     if(online.size >= 시작인원) {
         상태 = 플레이
         start()
